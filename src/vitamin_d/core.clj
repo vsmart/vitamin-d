f45951c0f370bdb8c9b999ff05a7d3d85aafa669
lislis <mail@lislis.de>
2016-05-03 21:14:21 +0200

    update slots

--- src/vitamin_d/core.clj
+++ src/vitamin_d/core.clj
@@ -2,6 +2,13 @@
   (:require [quil.core :refer :all]
             [quil.middleware :as m]))
 
+
+(defn generate-slot []
+  (rand-int 5))
+
+(defn update-slots [state]
+  (assoc state :slots [(generate-slot) (generate-slot) (generate-slot)]))
+
 (defn setup []
   {:countdown 100
    :stats 
@@ -11,14 +18,18 @@
 
 (defn draw [state]
   (background 255)
-  (fill 192)
+  (fill 50)
   (ellipse 100 100 30 30)
   (text (str "Time left: " (:countdown state)) 250 50)
   (text (str "Mood: " (:mood (:stats state))) 200 100)
-  (text (str "Success: " (:success (:stats state))) 300 100))
+  (text (str "Success: " (:success (:stats state))) 300 100)
+  (text (str (:slots state)), 250 200))
 
 (defn update [state]
-  state)
+  (update-slots state))
+  
+  
+
 
 (defsketch example
   :title "Vitamin D"
