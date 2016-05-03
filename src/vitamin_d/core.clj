1d93eff2d43eccaf4c2d92092a30b74655c7043c
lislis <mail@lislis.de>
2016-05-03 20:52:15 +0200

    draw some state

--- src/vitamin_d/core.clj
+++ src/vitamin_d/core.clj
@@ -1,18 +1,31 @@
 (ns vitamin-d.core
-  (:require [quil.core :refer :all]))
+  (:require [quil.core :refer :all]
+            [quil.middleware :as m]))
 
 (defn setup []
-  (smooth))
+  {:countdown 100
+   :stats 
+     {:mood 50 
+      :success 50}
+   :slots [0 0 0]})
 
-(defn draw []
+(defn draw [state]
   (background 255)
   (fill 192)
-  (ellipse 100 100 30 30))
+  (ellipse 100 100 30 30)
+  (text (str "Time left: " (:countdown state)) 250 50)
+  (text (str "Mood: " (:mood (:stats state))) 200 100)
+  (text (str "Success: " (:success (:stats state))) 300 100))
+
+(defn update [state]
+  state)
 
 (defsketch example
-  :title "Example"
+  :title "Vitamin D"
   :setup setup
   :draw draw
-  :size [200 200])
+  :update update
+  :size [600 600]
+  :middleware [m/fun-mode])
 
 (defn -main [& args])
