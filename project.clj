d71798a9717ecc338525d177b6c435d3671d07bf
lislis <mail@lislis.de>
2016-05-09 21:44:37 +0200

    update project description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject vitamin-d "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Decide where you spend your time"
+  :url "https://github.com/vsmart/vitamin-d/"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [quil "2.2.6" :exclusions [org.clojure/clojure]]]
   :main vitamin-d.core
